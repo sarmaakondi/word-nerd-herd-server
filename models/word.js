3d0601ef9c2a0d6c14b41300ee10021cadf684d9
Charlie Jay <charliejbjay@gmail.com>
2024-07-13 15:46:41 +1000

    format changes to word model

--- models/word.js
+++ models/word.js
@@ -1,22 +1,25 @@
 const mongoose = require("mongoose");
 
-const wordSchema = new mongoose.Schema({
-    word: {
-        type: String,
-        required: true,
+const wordSchema = new mongoose.Schema(
+    {
+        word: {
+            type: String,
+            required: true,
+        },
+        startsWith: {
+            type: String,
+            required: true,
+        },
+        meaning: {
+            type: String,
+            required: true,
+        },
+        examples: {
+            type: [String],
+            required: true,
+        },
     },
-    startsWith: {
-        type: String,
-        required: true,
-    },
-    meaning: {
-        type: String,
-        required: true,
-    },
-    examples: {
-        type: [String],
-        required: true,
-    },
-}, { timestamps: true });
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("Word", wordSchema);
