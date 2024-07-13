514eff388a524b2ee0d0a4b33d925b548810ee24
Charlie Jay <charliejbjay@gmail.com>
2024-07-13 15:43:09 +1000

    added timestamps to word model

--- models/word.js
+++ models/word.js
@@ -17,6 +17,6 @@ const wordSchema = new mongoose.Schema({
         type: [String],
         required: true,
     },
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model("Word", wordSchema);
