e4fabfdea0903529c6c14b3e0431df940bc71b24
avnms <avnmsarma@gmail.com>
2024-07-13 16:36:34 +1000

    Updated User model to login with email instead of username

--- models/user.js
+++ models/user.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
     {
-        username: {
+        email: {
             type: String,
             unique: true,
+            required: true,
+        },
+        username: {
+            type: String,
             required: true,
         },
         hashedPassword: {
