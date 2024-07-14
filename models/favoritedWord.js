5559673e0a05be84b33d7421957325642b813930
benjolyman <benjolyman@hotmail.com>
2024-07-15 09:47:42 +1000

    merge issue resolved

--- models/favoritedWord.js
+++ models/favoritedWord.js
@@ -1,21 +1,20 @@
 const mongoose = require("mongoose");
 
 const favoritedWordSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        word: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Word",
+            required: true,
+        },
     },
-    word: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Word",
-      required: true
-    },
-  },
 
-  { timestamps: true }
+    { timestamps: true }
 );
-
 
 module.exports = mongoose.model("FavoritedWord", favoritedWordSchema);
