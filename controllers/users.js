880a873049db2f66106a32067fa2fa16d53a90d1
benjolyman <benjolyman@hotmail.com>
2024-07-14 12:50:00 +1000

    fav words model created

--- controllers/users.js
+++ controllers/users.js
@@ -1,39 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
-
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const SALT_LENGTH = 12;
 
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
     try {
-        // Check if the username is already taken
-        const userInDatabase = await User.findOne({ username: req.body.username });
+        // Check if the email is already taken
+        const userInDatabase = await User.findOne({
+            email: req.body.email,
+        });
         if (userInDatabase) {
-            return res.json({error: 'Username already taken.'});
+            return res.json({ error: "email already taken." });
         }
         // Create a new user with hashed password
         const user = await User.create({
+            email: req.body.email,
             username: req.body.username,
-            hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH)
-        })
-        const token = jwt.sign({ username: user.username, _id: user._id }, process.env.JWT_SECRET);
+            hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH),
+        });
+        const token = jwt.sign(
+            { email: user.email, _id: user._id, username: user.username },
+            process.env.JWT_SECRET
+        );
         res.status(201).json({ user, token });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 });
 
-router.post('/signin', async (req, res) => {
+router.post("/signin", async (req, res) => {
     try {
-        const user = await User.findOne({ username: req.body.username });
-        if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
-            const token = jwt.sign({ username: user.username, _id: user._id }, process.env.JWT_SECRET);
+        const user = await User.findOne({ email: req.body.email });
+        if (
+            user &&
+            bcrypt.compareSync(req.body.password, user.hashedPassword)
+        ) {
+            const token = jwt.sign(
+                { email: user.email, _id: user._id, username: user.username },
+                process.env.JWT_SECRET
+            );
             res.status(200).json({ token });
         } else {
-            res.status(401).json({ error: 'Invalid username or password.' });
+            res.status(401).json({ error: "Invalid email or password." });
         }
     } catch (error) {
         res.status(400).json({ error: error.message });
--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -1,8 +1,8 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next) {
     try {
-        const token = req.headers.authorization.split(' ')[1];
+        const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         // Assign decoded payload to req.user
         req.user = decoded;
@@ -10,7 +10,7 @@ function verifyToken(req, res, next) {
         next();
     } catch (error) {
         // If any errors, send back a 401 status and an 'Invalid token.' error message
-        res.status(401).json({ error: 'Invalid authorization token.' });
+        res.status(401).json({ error: "Invalid authorization token." });
     }
 }
 
