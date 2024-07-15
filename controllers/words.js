2ee980a54a5c7c015e13ff0682d55c20b1dbe5cf
Charlie Jay <charliejbjay@gmail.com>
2024-07-15 10:48:11 +1000

    implemented get all words route

--- controllers/words.js
+++ controllers/words.js
@@ -9,10 +9,10 @@ const router = express.Router();
 router.get("/", async (req, res) => {
     const response = {};
     try {
-        const words = await Word.find({}).limit(5);
-        response["words"] = JSON.parse(words);
-        // response["count"] = .length();
-        res.status(200).send(response);
+        const words = await Word.find({}).limit(10);
+        response["words"] = words;
+        response["count"] = response["words"].length;
+        res.status(200).json(response);
     } catch (error) {
         res.status(500).json(error.message);
     }
