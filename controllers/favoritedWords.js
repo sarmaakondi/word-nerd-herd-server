68d45b2a0f429f0a72810e39824e17ef0dd2d964
benjolyman <benjolyman@hotmail.com>
2024-07-15 15:28:58 +1000

    unfavorite wip

---
+++ controllers/favoritedWords.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const verifyToken = require("../middleware/verify-token");
+
+const favoritedWord = require("../models/favoritedWord");
+
+router.use(verifyToken);
+
+//POST FAVORITED WORD
+router.post("/:id", async (req, res) => {
+    try {
+        const query = { user: req.user._id, word: req.params.id };
+        const existingFavoritedWord = await favoritedWord.find(query);
+        if (existingFavoritedWord.length > 0)
+            return res.status(200).json({
+                message: "The current word is already marked as favorited.",
+            });
+        const response = await favoritedWord.create({
+            user: req.user._id,
+            word: req.params.id,
+        });
+        res.status(201).json(response);
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+});
+
+// GET ALL FAVORITED WORDS
+router.get("/words", async (req, res) => {
+    const response = {};
+    try {
+        const query = { user: req.user._id };
+        const favoritedWords = await favoritedWord.find(query).limit(10);
+        response["favoritedWords"] = favoritedWords;
+        response["count"] = response["favoritedWords"].length;
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+});
+
+//GET ALL FAVORITED WORDS COUNT
+router.get("/count", async (req, res) => {
+    try {
+        const query = { user: req.user._id };
+        const response = await favoritedWord.countDocuments(query);
+        res.status(200).json({ count: response });
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+});
+
+//UNFAVORITE A WORD
+router.delete("/:id", async (req, res) => {
+    try {
+        const query = { user: req.user._id, word: req.params.id };
+        const existingFavoritedWord = await favoritedWord.find(query);
+        if (existingFavoritedWord.length === 0)
+            return res.status(200).json({
+                message: "The current word has not been favorited yet.",
+            });
+        const response = await existingFavoritedWord.deleteOne();
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const profilesRouter = require("./controllers/profiles");
 
 const wordsController = require("./controllers/words");
 const learnedWordsController = require("./controllers/learnedWords");
+const favoritedWordsController = require("./controllers/favoritedWords");
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -24,6 +25,7 @@ app.use("/users", usersRouter);
 app.use("/profiles", profilesRouter);
 app.use("/words", wordsController);
 app.use("/learnedWords", learnedWordsController);
+app.use("/favoritedWords", favoritedWordsController);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
