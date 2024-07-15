ab68ca054a99d5dec1752e6d7b4e30c3b9770a2f
Charlie Jay <charliejbjay@gmail.com>
2024-07-15 10:57:55 +1000

    implemented get single word route

--- controllers/words.js
+++ controllers/words.js
@@ -19,5 +19,15 @@ router.get("/", async (req, res) => {
 });
 
 // GET ONE WORD
+router.get("/:id", async (req, res) => {
+    try {
+        const response = await Word.findById(req.params.id);
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+});
+
+// We may not actually need to call this router in the front end.
 
 module.exports = router;
